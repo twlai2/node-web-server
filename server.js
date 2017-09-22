9d749ba571a512fc90e0cfaf68c2d32314a3daf3
Swan <swan@yakoo.com.hk>
2017-09-22 17:25:33 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    const log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log+'\n', (err) => {
+        if (err) console.log(err);
+    })
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+   res.render('home', {
+        welcomeMessage: 'Welcome to our Home',
+        pageTitle: 'Home 11',
+        currentYear: new Date().getFullYear(),
+   });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        pageTitle: 'About Page 2',
+        currentYear: new Date().getFullYear(),
+    });
+});
+
+app.get('/bad', (req, res) => {
+    const obj = {
+        errorMessage: 'Unable to fulfill this request'
+    }
+    res.send(obj);
+})
+
+app.listen(3001, () => {
+    console.log('Server is now up.');
+});
